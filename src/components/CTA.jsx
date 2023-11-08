c62a5b1f2cb0c3f0803a3ec30c7bf1d73cf3fa03
Abhishek Singh <abhinav210702@gmail.com>
2023-11-09 00:26:07 +0530

    fontchanged and style changed

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -2,11 +2,11 @@
 
 export const CTA = () => {
   return (
-    <div className="text-[#020826] mt-[7em]">
+    <div className="text-[#00214d] mt-[7em]">
         <div className="flex flex-col justify-center items-center">
-            <h1 className="font-lora font-bold text-3xl md:text-4xl">I&apos;m open to work.</h1>
-            <p className="font-nunito text-[#716040] md:text-xl text-center py-4 px-[1em]">If you like what you see, then let&apos;s work together. Get in touch!</p>
-            <a href="mailto:abhinav210702@gmail.com"><p className="md:text-2xl md:mt-[2em] font-inter text-[#020826] mt-[1em] underline cursor-pointer text-lg mb-[1em] ">abhinav210702@gmail.com</p></a>
+            <h1 className="font-syne font-bold text-3xl md:text-4xl">I&apos;m open to work.</h1>
+            <p className="font-syne text-[#1b2d45] md:text-xl text-center py-4 px-[1em]">If you like what you see, then let&apos;s work together. Get in touch!</p>
+            <a href="mailto:abhinav210702@gmail.com"><p className="md:text-2xl md:mt-[2em] font-syne text-[#020826] mt-[1em] underline cursor-pointer text-lg mb-[1em] ">abhinav210702@gmail.com</p></a>
         </div>
     </div>
   )
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,28 +1,28 @@
-import one from "../assets/pexels-monstera-production-6289170.webp"
-// import two from "../assets/pexels-jaymantri-4805.jpg"
-import three from "../assets/pexels-medhat-ayad-383568.webp"
-import four from "../assets/japan.webp"
-import five from "../assets/catsndogs.webp"
-import six from "../assets/pexels-mike-bird-1100008.webp"
+import one from "../assets/blogapp.webp"
+import two from "../assets/pexels-medhat-ayad-383568.webp"
+import three from "../assets/clientportfolio.webp"
+import four from "../assets/codekaro.webp"
+import five from "../assets/cryptorush.webp"
+import six from "../assets/opendevcons.webp"
 import { Link } from "react-router-dom"
 
 const Work = () => {
   return (
-    <div className='bg-[#f2f4f6] text-[#020826] pt-7 flex gap-10 px-4 md:px-0 flex-col justify-center items-center'>
+    <div className='bg-[#f2f4f6] text-[#020826] pt-10 flex gap-10 px-4 md:px-0 flex-col justify-center items-center'>
       <div className="flex gap-10 flex-col md:flex-row justify-center items-center">
       <Link to="https://craftzblog.vercel.app"><div className="relative group cursor-pointer">
-          <img src={six} alt="" className=" md:w-[500px] md:h-[400px] w-[350px] object-cover"/>
+          <img src={five} alt="" className=" md:w-[500px] md:h-[400px] w-[350px] object-cover"/>
           <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-[#f9f4ef] bg-opacity-50 transition-opacity duration-300 group-hover:opacity-100">
-            <p className="text-[#020826] font-archivo font-bold text-3xl md:text-5xl text-center uppercase">Blog Application</p>
-            <p className="text-[#020826] font-fira font-semibold mt-7 text-xs md:text-md text-center uppercase">CRUD React Application</p>
+            <p className="text-[#020826] font-syne font-bold text-3xl md:text-5xl text-center uppercase">CryptoRush</p>
+            <p className="text-[#020826] font-syne font-regular mt-7 text-xs md:text-md text-center uppercase">React JS App</p>
           </div>
         </div></Link>
 
         <Link to="https://cryptorushh.netlify.app"><div className="relative group">
           <img src={one} alt="" className="md:w-[100%] md:h-[400px] w-[350px] object-cover"/>
           <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-[#f9f4ef] bg-opacity-50 transition-opacity duration-300 group-hover:opacity-100">
-          <p className="text-[#020826] font-archivo font-bold text-3xl md:text-5xl text-center uppercase">CryptoRush</p>
-            <p className="text-[#020826] font-fira font-semibold mt-7 text-xs md:text-md text-center uppercase">React js Application</p>
+          <p className="text-[#020826] font-syne font-bold text-3xl md:text-5xl text-center uppercase">Blog App</p>
+            <p className="text-[#020826] font-syne  mt-7 text-xs md:text-md text-center uppercase">CRUD React JS App</p>
           </div>
         </div></Link>
         
@@ -30,18 +30,18 @@ const Work = () => {
 
       <div className="flex gap-10 flex-col md:flex-row justify-center items-center">
       <Link to="https://opendevcons.netlify.app/"><div className="relative group">
-        <img src={three} alt="" className="md:w-[100%] md:h-[400px] w-[350px] object-cover" />
+        <img src={six} alt="" className="md:w-[100%] md:h-[400px] w-[350px] object-cover" />
           <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-[#f9f4ef] bg-opacity-50 transition-opacity duration-300 group-hover:opacity-100">
-          <p className="text-[#020826] font-archivo font-bold text-3xl md:text-5xl text-center uppercase">Open Dev Cons</p>
-            <p className="text-[#020826] font-fira font-semibold mt-7 text-xs md:text-md text-center uppercase">Full Stack React Application</p>
+          <p className="text-[#020826] font-syne font-bold text-3xl md:text-5xl text-center uppercase">Open Dev Cons</p>
+            <p className="text-[#020826] font-syne  mt-7 text-xs md:text-md text-center uppercase">Full Stack React Application</p>
           </div>
         </div></Link>
 
         <Link to="https://codeekaro.netlify.app/"><div className="relative group">
         <img src={four} alt="" className=" md:w-[500px] md:h-[400px] w-[350px] object-cover"/>
           <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-[#f9f4ef] bg-opacity-50 transition-opacity duration-300 group-hover:opacity-100">
-          <p className="text-[#020826] font-archivo font-bold text-3xl md:text-5xl text-center uppercase">CodeKaro</p>
-            <p className="text-[#020826] font-fira font-semibold mt-7 text-xs md:text-md text-center uppercase">Front-End Application with API </p>
+          <p className="text-[#020826] font-syne font-bold text-3xl md:text-5xl text-center uppercase">CodeKaro</p>
+            <p className="text-[#020826] font-syne  mt-7 text-xs md:text-md text-center uppercase">Front-End Application with API </p>
           </div>
         </div></Link>
         
@@ -50,18 +50,18 @@ const Work = () => {
 
       <div className="flex gap-10 flex-col md:flex-row justify-center items-center">
       <Link to="https://enkrypttchat.netlify.app/"><div className="relative group">
-          <img src={five} alt="" className="md:w-[500px] md:h-[400px] w-[350px] object-cover" />
+          <img src={two} alt="" className="md:w-[500px] md:h-[400px] w-[350px] object-cover" />
           <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-[#f9f4ef] bg-opacity-50 transition-opacity duration-300 group-hover:opacity-100">
-          <p className="text-[#020826] font-archivo font-bold text-3xl md:text-5xl text-center uppercase">KryptoChat</p>
-            <p className="text-[#020826] font-fira font-semibold mt-7 text-xs md:text-md text-center uppercase">Full stack Real-time chat Application</p>
+          <p className="text-[#020826] font-syne font-bold text-3xl md:text-5xl text-center uppercase">KryptoChat</p>
+            <p className="text-[#020826] font-syne  mt-7 text-xs md:text-md text-center uppercase">Full stack Real-time chat Application</p>
           </div>
         </div></Link>
 
         <Link to="https://client-portfolio-nine.vercel.app/" ><div className="relative group">
-          <img src={six} alt="" className="md:w-[100%] md:h-[400px] w-[350px] object-cover"/>
+          <img src={three} alt="" className="md:w-[100%] md:h-[400px] w-[350px] object-cover"/>
           <div className="absolute inset-0 flex flex-col items-center justify-center opacity-0 bg-[#f9f4ef] bg-opacity-50 transition-opacity duration-300 group-hover:opacity-100">
-          <p className="text-[#020826] font-archivo font-bold text-3xl md:text-5xl text-center">Portfolio</p>
-            <p className="text-[#020826] font-fira font-semibold mt-7 text-xs md:text-md text-center uppercase" >Freelance work</p>
+          <p className="text-[#020826] font-syne font-bold text-3xl md:text-5xl text-center uppercase">Portfolio</p>
+            <p className="text-[#020826] font-syne mt-7 text-xs md:text-md text-center uppercase" >Freelance work</p>
           </div>
         </div></Link>
         
