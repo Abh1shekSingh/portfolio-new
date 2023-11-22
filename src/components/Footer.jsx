aa080fa7053d0825f0ab5061c5ee0354a5ecee13
Abhishek Singh <abhinav210702@gmail.com>
2023-11-22 14:38:43 +0530

    few UX changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,8 +12,8 @@ export const Footer = () => {
     <section>
       <div className="py-4 px-3 flex justify-between items-center">
         <h3 className="uppercase font-saira font-bold">&copy; {year} Craftingbugs</h3>
-        <h3 className="uppercase font-saira font-medium">Developed with ❤️</h3>
-        <h3 className="uppercase font-saira font-bold cursor-pointer flex items-center gap-5" onClick={scrollToTop}>Scroll to Top<FaRegArrowAltCircleUp /></h3>
+        <h3 className="uppercase font-saira font-medium text-center">Developed with ❤️</h3>
+        <h3 className="uppercase font-saira font-bold cursor-pointer flex items-center text-right gap-5" onClick={scrollToTop}>Scroll to Top<FaRegArrowAltCircleUp /></h3>
       </div>
     </section>
   )
